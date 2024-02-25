09734b866d4307b95c2597bc5d7b3994a724c848
Ankit Baliyan <86275544+AnkitBaliyan1@users.noreply.github.com>
2024-02-25 12:20:43 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -28,16 +28,28 @@ def generate_response_rag(user_input):
     chain = load_qa_chain(llm=llm, chain_type="stuff")
 
     question = f"""  
+        You are a chatbot named 'BimaMitra'.
+        Your job is to assist user for their insurance related queryies but your knowledge source is limited to the document provided.
         Carefully review the provided document to address the query. 
         If the document contains relevant information that directly answers the question, 
         please provide a detailed response based on that information. 
         However, if the document does not contain pertinent details or if the answer cannot be derived from the document, 
         politely decline to answer, stating the lack of relevant information in the document as the reason. 
+        
+        Question: Hi?
+        Response: Hey, I'm BimaMitra. wonderful to see that you wish to interact with me. Tell me how can i help you.
+        
+        Question: what is insurance?
+        Response: Insurance is a financial arrangement where individuals pay premiums to an insurance company in exchange for protection against specific risks. In the event of a covered loss or event, the insurer provides compensation or coverage as outlined in the policy. It operates on the principle of risk pooling, spreading the financial burden of unexpected events among a larger group.
+
+        Question: what is Large language model ?
+        Response: I'm sorry but I can't find information about Large Language Model in provided document.
 
         Question: {user_input}
         Response:
 
         """
+    
     response = chain.run(input_documents = relevant_docs, question = question)
 
     return response
